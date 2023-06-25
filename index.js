37264cc08c7a5559f61c9acd4bbd1fd3ba36ebde
vinaysingh <vinaysingh@rapidinnovation.dev>
2023-06-25 19:50:10 +0530

    added code for the voice chat bot

---
+++ index.js
@@ -0,0 +1,84 @@
+require("dotenv").config();
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+const multer = require("multer");
+const speech = require("@google-cloud/speech");
+const { Translate } = require("@google-cloud/translate").v2;
+const { v4: uuidv4 } = require("uuid");
+const { Configuration, OpenAIApi } = require("openai");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+// Conversation history
+let conversationHistory = [];
+
+// Handle the voice input processing
+app.post("/process", async (req, res) => {
+  try {
+    const transcript = req.body.transcript;
+
+    // Add user message to the conversation history
+    conversationHistory.push({ role: "user", content: transcript });
+
+    // Generate response from GPT
+    const chatCompletion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: conversationHistory,
+    });
+    const responseText = chatCompletion.data.choices[0].message?.content;
+
+    // Add AI response to the conversation history
+    conversationHistory.push({ role: "assistant", content: responseText });
+
+    const audio = await textToSpeech(responseText);
+
+    res.status(200).json({ audio, responseText, conversationHistory });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
+async function textToSpeech(text) {
+  const encodedParams = new URLSearchParams();
+  encodedParams.set("src", text);
+  encodedParams.set("hl", "en-us");
+  encodedParams.set("r", "0");
+  encodedParams.set("c", "mp3");
+  encodedParams.set("f", "32khz_8bit_mono");
+  encodedParams.set("b64", true);
+
+  const options = {
+    method: "POST",
+    url: "https://voicerss-text-to-speech.p.rapidapi.com/",
+    params: {
+      key: process.env.VOICE_RSS,
+    },
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+      "X-RapidAPI-Host": "voicerss-text-to-speech.p.rapidapi.com",
+    },
+    data: encodedParams,
+  };
+  const response = await axios.request(options);
+  console.log(response.data);
+  return response.data;
+}
---
+++ public/main.js
@@ -0,0 +1,66 @@
+const resultDiv = document.getElementById("result");
+const result2Div = document.getElementById("result2");
+const startButton = document.getElementById("startButton");
+let audioBuffer;
+
+// Create a new SpeechRecognition object
+const recognition = new webkitSpeechRecognition();
+recognition.continuous = true;
+let isRecording = false; // Track recording state
+
+// Event listener for when speech recognition results are available
+recognition.onresult = async (event) => {
+  if (!isRecording) return; // Skip if not recording
+
+  const { transcript } = event.results[event.results.length - 1][0];
+  console.log("Speech to text:", transcript);
+
+  // Display the transcribed text on the web page
+  resultDiv.textContent = transcript;
+
+  try {
+    // Send the transcript to the server for processing
+    const response = await fetch("/process", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ transcript }),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      const { audio, responseText } = data;
+      // Display the transcribed text on the web page
+      result2Div.textContent = responseText;
+
+      // Stop any currently playing audio
+      Howler.stop();
+
+      // Play the new audio buffer
+      const sound = new Howl({
+        src: [audio],
+        autoplay: true,
+      });
+      sound.play();
+    } else {
+      throw new Error("Request failed");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
+
+// Event listener for the start button click
+startButton.addEventListener("click", () => {
+  if (!isRecording) {
+    isRecording = true; // Start recording
+    recognition.start(); 
+    startButton.textContent = "Stop Recording";
+  } else {
+    recognition.stop()
+    isRecording = false; // Stop recording
+    startButton.textContent = "Start Recording";
+    resultDiv.textContent = ""; // Clear displayed transcript
+  }
+});
